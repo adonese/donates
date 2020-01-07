9fea7b50621e12769e28a8be52eea6a3884fc119
Mohamed Yousif <mmbusif@gmail.com>
2020-01-07 19:01:33 +0200

    fixing building script

--- script.sh
+++ script.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 yarn build
 sed -i 's/My page/Donation Platform/g' build/index.html
-cp logo_.png build
+cp $1 build/
+echo "the item is " $1
 
 sed -i 's/favicon.ico/logo.png/g' build/index.html
