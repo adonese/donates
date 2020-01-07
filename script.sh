657c7bb18c810bc5dc0450dc9739b8f1c1ef0db9
Mohamed Yousif <mmbusif@gmail.com>
2020-01-07 16:20:10 +0200

    new image

--- script.sh
+++ script.sh
@@ -3,4 +3,4 @@ yarn build
 sed -i 's/My page/Donation Platform/g' build/index.html
 cp logo_.png build
 
-sed -i 's/favicon.ico/logo_.png/g' build/index.html
+sed -i 's/favicon.ico/logo.png/g' build/index.html
